2507452008c239f02bb6b1932704049bf0ed57f4
Wagner Palmeira <wagnerpalmeira.ti@gmail.com>
2025-01-14 19:03:26 -0300

    Sample of spring + elk

---
+++ spring-log-elk-project/spring-log-elk/src/main/java/br/com/lowlevel/spring_log_elk/SpringLogElkApplication.java
@@ -0,0 +1,13 @@
+package br.com.lowlevel.spring_log_elk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLogElkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLogElkApplication.class, args);
+	}
+
+}
---
+++ spring-log-elk-project/spring-log-elk/src/test/java/br/com/lowlevel/spring_log_elk/SpringLogElkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lowlevel.spring_log_elk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLogElkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
