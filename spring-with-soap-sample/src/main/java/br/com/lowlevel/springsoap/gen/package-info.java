c58e39b272e638a06dd5f8d26e8803963a318fad
Wagner <wagnerpalmeira.ti@gmail.com>
2025-03-14 15:53:58 -0300

    Adding samples

---
+++ spring-with-soap-sample/src/main/java/br/com/lowlevel/springsoap/gen/package-info.java
@@ -0,0 +1,9 @@
+//
+// Este arquivo foi gerado pela Eclipse Implementation of JAXB, v3.0.0 
+// Consulte https://eclipse-ee4j.github.io/jaxb-ri 
+// Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
+// Gerado em: 2024.10.26 às 01:52:15 PM BRT 
+//
+
+@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://www.lowlevel.com.br/springsoap/gen", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package br.com.lowlevel.springsoap.gen;
